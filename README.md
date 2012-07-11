b5857b7ca6ae4289987f2d0a050a69ac4dd8b763
Darren DeRidder <drderidder@gmail.com>
2012-07-11 13:31:51 -0400

    Replacing some recent additions with simpler alternative. Resolves issue 4. Adds "push" command to Selenium IDE; pushes new value to stored array, creating new array if necessary.

--- README.md
+++ README.md
@@ -23,37 +23,7 @@ Resources
 - Install and usage information [Blog](http://51elliot.blogspot.com/2008/02/selenium-ide-goto.html)
 - Selenium HQ [SeleniumHQ](http://seleniumhq.org/)
 
-
-ForEach loop enhancement by Martin Bramwell
----
-
-With this enhancement you can now perform For Each loops on a previously stored collection.
-
-<img src="https://github.com/martinhbramwell/SeleniumForOpenERP/raw/master/scrapheap/SeleniumForEach.png"></img>
-
-You should have no difficulty adapting to your needs the example in ./demos/testForEach.html
-
-The steps are :
-<pre>
-|addCollection   |  aSuitableCollectionName           |                                  |
-|addToCollection |  aSuitableCollectionName           |  aValue                          |
-|addToCollection |  aSuitableCollectionName           |  aDifferentValue                 |
-|addToCollection |  aSuitableCollectionName           |  anotherValue                    |
-|storeFor        |  aSuitableCollectionName           |  aSuitableTemporaryVariableName  |
-|echo            |  ${aSuitableTemporaryVariableName} |                                  |
-|endFor          |                                    |                                  |
-</pre>
-
-* addCollection - instantiate a collection
-* addToCollection - adds a value to the indicated collection (JSON objects are ok)
-* storeFor - retrieves the next available value from your collection and stuffs it into the indicated temporary variable. If there's no value available it jumps to the endFor.
-* endFor - marks the end of the execution block
-
 Authors
 ---
-- Andrey Yegorov - Created the original flow control extension
-- Darren DeRidder - Modified the original extension for Selenium IDE (http://51elliot.blogspot.com/2008/02/selenium-ide-goto.html)
-- Adam Goucher - Author of the Selenium IDE plugin API 
-- Dave Hunt - All I did was turn the extension into a plugin
-- Martin "Hasan" Bramwell |  http://openerp24hrs.blogspot.com/  |
-- Martin "Hasan" Bramwell - Added For/Each looping (https://github.com/martinhbramwell/sideflow)
+- Andrey Yegorov - original flow control extension
+- Darren DeRidder - Selenium IDE port (http://51elliot.blogspot.com/2008/02/selenium-ide-goto.html)
--- sideflow.js
+++ sideflow.js
@@ -13,14 +13,6 @@ Selenium.prototype.reset = function() {
 
 
 /*
-This toolkit is the work of Andrey Yegorov & Darren DeRidder of the Ottawa office of Kindsight
-
-The ForEach stuff at the bottom below the separator is a contribution by Martin H. Bramwell, 2011/11
-*/
-
-var forEachLabels = {};  // mhb:20111107
-
-/*
  * ---   Initialize Conditional Elements  --- *
  *  Run through the script collecting line numbers of all conditional elements
  *  There are three a results arrays: goto labels, while pairs and forEach pairs
@@ -30,7 +22,6 @@ Selenium.prototype.initialiseLabels = function()
 {
     gotoLabels = {};
     whileLabels = { ends: {}, whiles: {} };
-    forEachLabels = { forends: {}, fors: {} };  // mhb:20111107
     var command_rows = [];
     var numCommands = testCase.commands.length;
     for (var i = 0; i < numCommands; ++i) {
@@ -49,8 +40,8 @@ Selenium.prototype.initialiseLabels = function()
             case "endwhile":
                 cycles.push( [command_rows[i].command.toLowerCase(), i] )
                 break;
-            case "storefor":
-            case "endfor":
+            case "foreach":
+            case "endforeach":
                 forEachCmds.push( [command_rows[i].command.toLowerCase(), i] )
                 break;
         }
@@ -76,28 +67,6 @@ Selenium.prototype.initialiseLabels = function()
         }
     }
 
-/*  ---- mhb: 20111107 --- begin --- */
-    var idxFE = 0;
-    while( forEachCmds.length ) {
-        if( idxFE >= forEachCmds.length ) {
-            throw new Error( "non-matching storeFor/endFor found" );
-        }
-        switch( forEachCmds[idxFE][0] ) {
-            case "storefor":
-                if( ( idxFE+1 < forEachCmds.length ) && ("endfor" == forEachCmds[idxFE+1][0]) ) {
-                    // pair found
-                    forEachLabels.forends[ forEachCmds[idxFE+1][1] ] = forEachCmds[idxFE][1];
-                    forEachLabels.fors[ forEachCmds[idxFE][1] ] = forEachCmds[idxFE+1][1];
-                    forEachCmds.splice( idxFE, 2 );
-                    idxFE = 0;
-                } else ++idxFE;
-                break;
-            case "endfor":
-                ++idxFE;
-                break;
-        }
-    }
-
 }
 
 Selenium.prototype.continueFromRow = function( row_num )
@@ -111,7 +80,7 @@ Selenium.prototype.continueFromRow = function( row_num )
 // do nothing. simple label
 Selenium.prototype.doLabel = function(){};
 
-Selenium.prototype.doGotolabel = function( label )
+Selenium.prototype.doGotoLabel = function( label )
 {
     if( undefined == gotoLabels[label] ) {
         throw new Error( "Specified label '" + label + "' is not found." );
@@ -119,11 +88,11 @@ Selenium.prototype.doGotolabel = function( label )
     this.continueFromRow( gotoLabels[ label ] );
 };
 
-Selenium.prototype.doGoto = Selenium.prototype.doGotolabel;
+Selenium.prototype.doGoto = Selenium.prototype.doGotoLabel;
 
 Selenium.prototype.doGotoIf = function( condition, label )
 {
-    if( eval(condition) ) this.doGotolabel( label );
+    if( eval(condition) ) this.doGotoLabel( label );
 }
 
 Selenium.prototype.doWhile = function( condition )
@@ -144,112 +113,13 @@ Selenium.prototype.doEndWhile = function()
     this.continueFromRow( while_row );
 }
 
-
-/* -------   ForEach looping Contributed by Martin "Hasan" Bramwell 2011/11/07  ------- */
-
-/*
- * Note.  I'm neither a JavaScript nor a Selenium pro.
- * There are surely some nice fix-ups that a real expert would spot in a minute.
- * If you have an idea of a better way please let me know.
- * Some 'nice-to-have' ideas :
- * -- a 'For' command instead of a 'storeFor' command
- * -- nested ForEach
- * -- 
- */
-
-	var iteratorsMap = {};
-	var collectionsMap = {};
-
-	Selenium.prototype.getFor = function( _nameCollection )
-	{
-
-		  if( eval(iteratorsMap[_nameCollection].hasNext()) ) {
-				return iteratorsMap[_nameCollection].next();
-			} else {
-		      var last_foreach_row = testCase.debugContext.debugIndex;
-		      var end_for_row = forEachLabels.fors[ last_foreach_row ];
-		      if( undefined == end_for_row ) throw new Error( "Corresponding 'endFor' could not be found." );
-		      this.continueFromRow( end_for_row );
-		  }
-	}
-
-	Selenium.prototype.doEndFor = function()
-	{
-		  var last_foreach_row = testCase.debugContext.debugIndex;
-		  var for_row = forEachLabels.forends[ last_foreach_row ] - 1;
-		  if( undefined == for_row ) throw new Error( "Corresponding 'storeFor' could not be found." );
-		  this.continueFromRow( for_row );
-	}
-
-
-
-
-	Selenium.prototype.doAddToCollection = function(_nameCollection, _valueToStore) {
-			var initialized = true;
-
-			if ((iteratorsMap == undefined) || (collectionsMap == undefined)) initialized = false;
-
-			if (collectionsMap[_nameCollection] == undefined)  initialized = false;
-
-			if (!initialized) throw new Error( "You must first call '|addCollection | " + _nameCollection + "||' in order to initialize it." );
-
-			collectionsMap[_nameCollection].add(_valueToStore);
-			iteratorsMap[_nameCollection] = collectionsMap[_nameCollection].iterator();
-
-	};
-
-
-	Selenium.prototype.doAddCollection = function(_nameCollection) {
-
-			if (iteratorsMap == undefined) {
-				iteratorsMap = new Array();
-			}
-
-			if (collectionsMap == undefined) {
-				collectionsMap = new Array();
-			}
-
-			collectionsMap[_nameCollection] = new Collection(new Array());
-
-	};
-
-	function Iterator(_collection)
-	{
-		var privateCollection = _collection;
-		var total = privateCollection.length;
-		var cursor = 0;
-
-		function doNext() {
-			return privateCollection[cursor++];
-		}
-
-		function doHasNext() {  return  cursor < total; }
-		function doRemainderCount() {  return  total - cursor; }
-
-		this.next = doNext;
-		this.hasNext = doHasNext;
-		this.remainderCount = doRemainderCount;
-
-	}
-
-
-	function Collection(_collection)
-	{
-		var privateCollection = _collection;
-
-		function doSize() { return  privateCollection.length; }
-		this.size = doSize;
-
-		function doIterator() {
-			return new Iterator(privateCollection);
-		}
-
-		function doAdd(_value) {
-			privateCollection[privateCollection.length] = _value;
-		}
-
-		this.iterator = doIterator;
-		this.add = doAdd;
-		
-	}
-
+Selenium.prototype.doPush= function(value, varName)
+{
+    if(typeof storedVars[varName] === 'undefined') {
+        storedVars[varName] = new Array();
+    } else if(typeof storedVars[varName] !== 'object') {
+        throw new Error("Cannot push value onto non-array " + varName);
+    } else {
+        storedVars[varName].push(value);
+    }
+}
